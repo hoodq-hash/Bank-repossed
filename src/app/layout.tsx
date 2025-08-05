5ae42afc9b5f7fc23835c9f4cc690fa30521173e
Joedev247 <blessednur67@gmail.com>
2025-08-05 13:24:02 +0100

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,24 +48,12 @@ export default function RootLayout({
       </head>
       <body className={`${outfit.variable} font-outfit antialiased`}>
         <Toaster richColors position="top-center" />
-        <Script id="tawkto" strategy="afterInteractive">
-          {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/68428d6ce312af190f1d47ee/1it1vgvao';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
+        <script
+          src="//code.tidio.co/oa9dz4dbhhkqvjmzvn0hthn5vyyggfng.js"
+          async
+        ></script>
         {children}
       </body>
     </html>
   );
 }
-
-
-
