4d65d3abbdc5bfc892348773cb41de650f3adc2f
BlessedNur <blessednur67@gmail.com>
2026-01-04 03:07:07 +0100

    done

--- src/app/api/cars/[id]/route.ts
+++ src/app/api/cars/[id]/route.ts
@@ -5,10 +5,9 @@ import SCar from "@/models/SCar";
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dwoaukreo",
-  api_key: process.env.CLOUDINARY_API_KEY || "378833648339572",
-  api_secret:
-    process.env.CLOUDINARY_API_SECRET || "11daIfpiOnJEsAUeCIKzKShVzSw",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // GET a specific car by ID
